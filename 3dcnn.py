972ef5f729de4b4be3d7a60fdcd49f1006d3da56
aneuman1114 <82843858+aneuman1114@users.noreply.github.com>
2025-03-12 13:05:27 -0400

    reading in data

--- 3dcnn.py
+++ 3dcnn.py
@@ -8,3 +8,9 @@ from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
+import numpy as np
+
+#here we are reading in a data file as a numpy array
+
+data1 = np.loadtxt("fp5chi10p9rhoSpecies0.dat", delimiter=' ', usecols=(0,1,2,3))
+print(data1)
