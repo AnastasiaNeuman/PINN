4f3903a1d3e6c0b4b7dfd3c6b8f4380384cbba07
aneuman1114 <82843858+aneuman1114@users.noreply.github.com>
2025-03-12 14:03:12 -0400

    created tensor dataset with labels

--- 3dcnn.py
+++ 3dcnn.py
@@ -5,8 +5,7 @@
 
 import torch
 from torch import nn
-from torch.utils.data import DataLoader
-from torchvision import datasets
+from torch.utils.data import TensorDataset, DataLoader
 from torchvision.transforms import ToTensor
 import numpy as np
 
@@ -38,3 +37,30 @@ tensor22 = torch.index_select(tensor2, 1, torch.LongTensor([2,0,1,3]))
 tensor23 = torch.index_select(tensor2, 1, torch.LongTensor([0,2,1,3]))
 tensor24 = torch.index_select(tensor2, 1, torch.LongTensor([1,0,2,3]))
 tensor25 = torch.index_select(tensor2, 1, torch.LongTensor([1,2,0,3]))
+
+#combine tensors into one data tensor
+data = torch.stack((tensor1, tensor11, tensor12, tensor13, tensor14, tensor15, tensor2, tensor21, tensor22, tensor23, tensor24, tensor25))
+#now we have a small set of data to start as a training set
+#next we have to create our labels
+
+labels_map = {
+    0: "Lamellar",
+    1: "Cylindrical",
+    2: "Spherical",
+    3: "Gyroid",
+    4: "Disordered",
+}
+
+#all the data should be lamellar, so we'll start with a basic label tensor of zeros
+labels = torch.zeros(12)
+
+#now let's try creating a custom dataset combining the tensors and labels
+dataset = TensorDataset(data, labels)
+print("Dataset sample:", dataset[1])
+
+
+
+
+
+
+
